2f5c47ee317e71a7e166f155b0fafd664d5d10d2
KoloDYS <kolody-serge1@yandex.ru>
2023-03-13 20:25:05 +0300

    Start Lesson2, add class Person

---
+++ Lesson2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String name = "Vladimir";
+    String sex = "male";
+    double height = 1.81;
+    int weight = 74;
+    int age = 25;
+
+    void go() {
+        System.out.println(name + " идет");
+    }
+
+    void sit() {
+        System.out.println(name + "сидит");
+    }
+
+    void run() {
+        System.out.println(name + " бегает");
+    }
+
+    void speak() {
+        System.out.println(name + "говорит: Мой возраст - " + age + " лет");
+    }
+
+    void learn() {
+        System.out.println(name + " учит Java");
+    }
+}
