4e4808b82f2f1c0d80465f098e851a92e2fd7305
KoloDYS <kolody-serge1@yandex.ru>
2023-03-16 22:22:07 +0300

    fix wolfTest: two files

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    public int weight;
+    public int age;
+    public char sex;
+    public String name;
+    public String color;
+
+    void go() {
+        System.out.println("\nИдет");
+    }
+
+    void sit() {
+        System.out.println("\nСидит");
+    }
+
+    void run() {
+        System.out.println("\nБежит");
+    }
+
+    void howl() {
+        System.out.println("\nВоет");
+    }
+
+    void hunt() {
+        System.out.println("\nОхотится");
+    }
+}
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -20,31 +20,3 @@ public class WolfTest {
         wolf.hunt();
     }
 }
-
-class Wolf {
-    public int weight;
-    public int age;
-    public char sex;
-    public String name;
-    public String color;
-
-    void go() {
-        System.out.println("\nИдет");
-    }
-
-    void sit() {
-        System.out.println("\nСидит");
-    }
-
-    void run() {
-        System.out.println("\nБежит");
-    }
-
-    void howl(){
-        System.out.println("\nВоет");
-    }
-
-    void hunt() {
-        System.out.println("\nОхотится");
-    }
-}
