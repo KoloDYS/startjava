a82b42a85b5e776b94d845e06c935abd36bc636c
KoloDYS <kolody-serge1@yandex.ru>
2023-03-18 01:26:42 +0300

    Final homework lesson 2: part 2 Jaeger

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,73 @@
+public class Jaeger {
+    private String mark;
+    private String origin;
+    private int height;
+    private int speed;
+    private int armor;
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void increaseSpeed(int speed) {
+        this.speed += speed;
+    }
+
+    public void decreaseSpeed(int speed) {
+        this.speed -= speed;
+    }
+
+    public void takeArmor() {
+        System.out.println("броня надета");
+    }
+
+    public void removeArmor() {
+        System.out.println("броня снята");
+    }
+
+    public Jaeger() {}
+
+    public Jaeger(String mark, String origin, int height, int speed, int armor) {
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.speed = speed;
+        this.armor = armor;
+    }
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,42 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger robot1 = new Jaeger();
+        Jaeger robot2 = new Jaeger("Mark-5", "Australia", 250, 10, 9);
+
+        robot1.setMark("Mark-3");
+        robot1.setOrigin("USA");
+        robot1.setHeight(260);
+        robot1.setSpeed(7);
+        robot1.setArmor(6);
+
+        System.out.println("Сравнение марок двух роботов: " +
+                robot1.getMark() + " и " +  robot2.getMark());
+
+        System.out.println("Скорость 1-го и 2-го роботов соответственно: " +
+                robot1.getSpeed() + " и " + robot2.getSpeed());
+
+        robot1.increaseSpeed(2);
+        robot2.decreaseSpeed(1);
+        System.out.println("После изменения скоростей: " + robot1.getSpeed() + " и " +
+                 robot2.getSpeed());
+
+        System.out.println("Состояние брони: ");
+        System.out.println("У робота №1: ");
+        robot1.takeArmor();
+        System.out.println("У робота №2: ");
+        robot2.removeArmor();
+        System.out.println("Изменено: ");
+        System.out.println("У робота №2: ");
+        robot2.takeArmor();
+
+        robot1.setArmor(9);
+        System.out.println("Значение брони у 1 робота после изменения: " + robot1.getArmor());
+
+        if (robot1.getArmor() == robot2.getArmor()) {
+            System.out.println("Значение брони у обоих роботов равно " + robot2.getArmor());
+        } else {
+            System.out.println("Значения роботов расходятся на: " +
+                    (robot1.getArmor() - robot2.getArmor()));
+        }
+    }
+}
