832d84696c6b06762c577fa9a94227d4068ec19a
KoloDYS <kolody-serge1@yandex.ru>
2023-03-29 21:04:45 +0300

    Array fix

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -8,57 +8,59 @@ public class ArrayTheme {
         int[] nums = {4, 2, 5, 7, 1, 3, 6};
         System.out.print("Массив до модификации: ");
         printElementsOfArray(nums);
-        for (int i = 0; i < nums.length / 2; i++) {
+        int len = nums.length;
+        for (int i = 0; i < len / 2 + 1; i++) {
             int temp = nums[i];
-            nums[i] = nums[nums.length - 1 - i];
-            nums[nums.length - 1 - i] = temp;
+            nums[i] = nums[--len];
+            nums[len] = temp;
         }
         System.out.print("\nМассив после модификации: ");
         printElementsOfArray(nums);
 
         System.out.println("\n\n2.Вывод произведения элементов массива");
-        int[] tenDigits = new int[10];
-        for (int i = 0; i < tenDigits.length; i++) {
-            tenDigits[i] = i;
+        int[] arrayOfDigits = new int[10];
+        len = arrayOfDigits.length;
+        for (int i = 0; i < len; i++) {
+            arrayOfDigits[i] = i;
         }
         int product = 1;
-        for (int i = 1; i < tenDigits.length - 1; i++) {
+        for (int i = 1; i < len - 1; i++) {
             product *= i;
-            System.out.print((i == 8) ? i + " = " + product : i + " * ");
+            System.out.print((arrayOfDigits[i] == len - 2) ? arrayOfDigits[i] + " = " + product :
+                    arrayOfDigits[i] + " * ");
         }
-        System.out.println("\nЦифры 0 и 9 находятся в массиве под индексами : " + tenDigits[0] + " и " + tenDigits[9]);
+        System.out.println("\nЦифры 0 и 9 находятся в массиве под индексами : " +
+                arrayOfDigits[0] + " и " + arrayOfDigits[9]);
 
         System.out.println("\n3.Удаление элементов массива");
         double[] doubleArr = new double[15];
-        int len = doubleArr.length;
+        len = doubleArr.length;
         for (int i = 0; i < len; i++) {
             doubleArr[i] = Math.random();
         }
-        int indexOfMid = len / 2;
-        double key = doubleArr[indexOfMid];
+        System.out.println("Массив до обнуления");
+        printDoubleArray(doubleArr);
+        double comparisonNum = doubleArr[len / 2];
         int counter = 0;
         for (int i = 0; i < len; i++) {
-            if (doubleArr[i] > key) {
+            if (doubleArr[i] > comparisonNum) {
                 doubleArr[i] = 0;
                 counter++;
             }
         }
-        for (int i = 0; i < len; i++) {
-            if (i == 8) {
-                System.out.println();
-            }
-            System.out.printf("%.3f  ", doubleArr[i]);
-        }
+        System.out.println("\nМассив после обнуления");
+        printDoubleArray(doubleArr);
         System.out.println("\nКоличество обнуленных ячеек = " + counter);
 
         System.out.println("\n4.Вывод элементов массива лесенкой в обратном порядке");
-        char[] chars = new char[26];
-        for (int i = 65, j = 0; i < 91; i++, j++) {
-            chars[j] = (char) i;
+        char[] letters = new char[26];
+        for (int i = 0; i < 26; i++) {
+            letters[i] = (char) ('A' + i);
         }
         String alphabet = "";
-        for (int i = chars.length - 1; i >= 0; i--) {
-            alphabet = alphabet + chars[i];
+        len = letters.length;
+        for (int i = len - 1; i >= 0; i--) {
+            alphabet += letters[i];
             System.out.println(alphabet);
         }
 
@@ -87,42 +89,46 @@ public class ArrayTheme {
         }
 
         System.out.println("\n\n6.Копирование не пустых строк");
-        String[] stringsAndSpaces = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
-        counter = 0;
-        for (String s : stringsAndSpaces) {
-            if (s.isBlank()) {
-                counter++;
+        String[] srcStrings = {"    ", "AA", "", "BBB", "CC", "D", "    ", "E", "FF", "G", ""};
+        len = srcStrings.length;
+        int countNonBlankStrings = 0;
+        for (String s : srcStrings) {
+            if (!s.isBlank()) {
+                countNonBlankStrings++;
             }
         }
-        String[] strings = new String[stringsAndSpaces.length - counter];
-        int startSequence = 0;
-        int endSequence = 0;
+        String[] destStrings = new String[countNonBlankStrings];
+        int startSequence = -1;
+        int endSequence;
         int desPos = 0;
-        //If two or more spaces
-        for (int i = 0; i < stringsAndSpaces.length; i++) {
-            if (stringsAndSpaces[i].isBlank()) {
-                if (startSequence == 0) {
-                    startSequence = i + 1;
-                } else {
-                    endSequence = i;
-                    System.arraycopy(stringsAndSpaces, startSequence, strings, desPos, endSequence - startSequence);
-                    desPos += (endSequence - startSequence);
-                    startSequence = endSequence + 1;
+        for (int i = 0; i < len; i++) {
+            if (!srcStrings[i].isBlank()) {
+                if (startSequence == -1) {
+                    startSequence = i;
                 }
+            } else if (srcStrings[i].isBlank() && startSequence != -1) {
+                endSequence = i;
+                System.arraycopy(srcStrings, startSequence, destStrings, desPos, endSequence - startSequence);
+                desPos += (endSequence - startSequence);
+                startSequence = endSequence + 1;
             }
         }
-        //If one space
-        if (endSequence == 0) {
-            System.arraycopy(stringsAndSpaces, 0, strings, 0, startSequence - 1);
-            System.arraycopy(stringsAndSpaces, startSequence, strings, startSequence - 1, (strings.length - startSequence + 1));
-        }
-        System.out.println("Первоначальный массив строк: " + Arrays.toString(stringsAndSpaces));
-        System.out.println("Измененный массив строк: " + Arrays.toString(strings));
+        System.out.println("Первоначальный массив строк: " + Arrays.toString(srcStrings));
+        System.out.println("Измененный массив строк: " + Arrays.toString(destStrings));
     }
 
     private static void printElementsOfArray(int[] array) {
         for (int num : array) {
             System.out.print(num + " ");
+        }
+    }
+
+    private static void printDoubleArray(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length / 2 + 1) {
+                System.out.println();
+            }
+            System.out.printf("%.3f  ", array[i]);
         }
     }
 }
