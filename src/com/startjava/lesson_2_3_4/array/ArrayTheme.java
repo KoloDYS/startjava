21efa4d7a5e8a19f34c43e52465113e91ad27a2c
KoloDYS <kolody-serge1@yandex.ru>
2023-04-01 12:38:55 +0300

    Array fix names, and product output

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -7,30 +7,29 @@ public class ArrayTheme {
         System.out.println("1.Реверс значений массива");
         int[] nums = {4, 2, 5, 7, 1, 3, 6};
         System.out.print("Массив до модификации: ");
-        printElementsOfArray(nums);
+         print(nums);
         int len = nums.length;
-        for (int i = 0; i < len / 2 + 1; i++) {
+        for (int i = 0; i <= len / 2; i++) {
             int temp = nums[i];
             nums[i] = nums[--len];
             nums[len] = temp;
         }
         System.out.print("\nМассив после модификации: ");
-        printElementsOfArray(nums);
+         print(nums);
 
         System.out.println("\n\n2.Вывод произведения элементов массива");
-        int[] arrayOfDigits = new int[10];
-        len = arrayOfDigits.length;
+        int[] multipliers = new int[10];
+        len = multipliers.length;
         for (int i = 0; i < len; i++) {
-            arrayOfDigits[i] = i;
+            multipliers[i] = i;
         }
         int product = 1;
         for (int i = 1; i < len - 1; i++) {
             product *= i;
-            System.out.print((arrayOfDigits[i] == len - 2) ? arrayOfDigits[i] + " = " + product :
-                    arrayOfDigits[i] + " * ");
+            System.out.print(multipliers[i] + ((multipliers[i] == len - 2) ? " = " + product :  " * "));
         }
         System.out.println("\nЦифры 0 и 9 находятся в массиве под индексами : " +
-                arrayOfDigits[0] + " и " + arrayOfDigits[9]);
+                multipliers[0] + " и " + multipliers[9]);
 
         System.out.println("\n3.Удаление элементов массива");
         double[] doubleArr = new double[15];
@@ -39,29 +38,29 @@ public class ArrayTheme {
             doubleArr[i] = Math.random();
         }
         System.out.println("Массив до обнуления");
-        printDoubleArray(doubleArr);
-        double comparisonNum = doubleArr[len / 2];
+        print(doubleArr);
+        double middleCellNum = doubleArr[len / 2];
         int counter = 0;
         for (int i = 0; i < len; i++) {
-            if (doubleArr[i] > comparisonNum) {
+            if (doubleArr[i] > middleCellNum) {
                 doubleArr[i] = 0;
                 counter++;
             }
         }
         System.out.println("\nМассив после обнуления");
-        printDoubleArray(doubleArr);
+        print(doubleArr);
         System.out.println("\nКоличество обнуленных ячеек = " + counter);
 
         System.out.println("\n4.Вывод элементов массива лесенкой в обратном порядке");
-        char[] letters = new char[26];
+        char[] alphabet = new char[26];
         for (int i = 0; i < 26; i++) {
-            letters[i] = (char) ('A' + i);
+            alphabet[i] = (char) ('A' + i);
         }
-        String alphabet = "";
-        len = letters.length;
+        String alphabetSequence = "";
+        len = alphabet.length;
         for (int i = len - 1; i >= 0; i--) {
-            alphabet += letters[i];
-            System.out.println(alphabet);
+            alphabetSequence += alphabet[i];
+            System.out.println(alphabetSequence);
         }
 
         System.out.println("\n5.Генерация уникальных чисел");
@@ -117,13 +116,13 @@ public class ArrayTheme {
         System.out.println("Измененный массив строк: " + Arrays.toString(destStrings));
     }
 
-    private static void printElementsOfArray(int[] array) {
+    private static void print(int[] array) {
         for (int num : array) {
             System.out.print(num + " ");
         }
     }
 
-    private static void printDoubleArray(double[] array) {
+    private static void print(double[] array) {
         for (int i = 0; i < array.length; i++) {
             if (i == array.length / 2 + 1) {
                 System.out.println();
