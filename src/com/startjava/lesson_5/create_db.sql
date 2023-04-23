d54851ce5ba4811111dfe110146b542b47828124
KoloDYS <kolody-serge1@yandex.ru>
2023-04-23 17:15:24 +0300

    Lesson 5 files

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS Jaegers;
+
+DROP DATABASE IF EXISTS robots;
+CREATE DATABASE robots;
+
+\c robots;
+/* dsdsdsdsdsd */
+CREATE TABLE Jaegers (
+ id SERIAL PRIMARY KEY,
+ modelName TEXT,
+ mark INTEGER,
+ height INTEGER,
+ weight DOUBLE PRECISION,
+ status TEXT,
+ origin TEXT,
+ launch DATE,
+ kaijuKill INTEGER);
+
+\ir init_db.sql;
+
+\ir queries.sql;
---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,36 @@
+\echo '--all table' 
+SELECT * FROM Jaegers;
+
+\echo '--only Active robots'
+SELECT * FROM Jaegers WHERE status = 'Active';
+
+\echo '--only robots 1 and 4 series' 
+SELECT * FROM Jaegers 
+WHERE mark IN ('1', '4');
+
+\echo '--all robots except 1 and 4 series'
+SELECT * FROM Jaegers 
+WHERE mark NOT IN('1', '4');
+
+\echo '--robots in desc order of series'
+SELECT * FROM Jaegers 
+ORDER BY mark DESC;
+
+\echo '--Info about the oldest robot'
+SELECT * FROM Jaegers 
+WHERE launch = (SELECT MIN(launch) FROM Jaegers);
+
+\echo '--robots that destroyed the most'
+SELECT * FROM Jaegers 
+WHERE kaijukill >= '5';
+
+\echo '--AVG weight of robots'
+SELECT AVG(weight) FROM Jaegers;
+
+\echo '--increment kajukill for active robots'
+UPDATE Jaegers SET kaijukill = kaijukill + 1 
+WHERE status = 'Active';
+
+\echo '--Delete all destroyed robots' 
+DELETE FROM Jaegers 
+WHERE status = 'Destroyed';
